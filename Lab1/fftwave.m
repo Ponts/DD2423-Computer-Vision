c63bdf84b233adb1846acd36e0aecc744b4ffc5d
Ponts <pbrink@kth.se>
2018-11-05 15:22:37 +0100

    init

---
+++ Lab1/fftwave.m
@@ -0,0 +1,56 @@
+function  fftwave(u,v, sz)
+    if (nargin < 2)
+        error('Requires at least two input arguments.');
+    end
+    if (nargin==2)
+        sz = 128;
+    end
+    Fhat = zeros(sz);
+    Fhat(u,v)=1;
+    
+    F = ifft2(Fhat);
+    Fabsmax = max(abs(F(:)));
+
+    subplot(3,2,1);
+    showgrey(Fhat);
+    title(sprintf('Fhat: (u,v) = (%d, %d)', u,v));
+    %What is done by these instructions?
+    %Because Matlab is 1-indexed we subtract 1.
+    %Because Matlab puts the center in the top left corner, we map
+    %frequencies 
+    if (u <= sz/2)
+        uc = u-1;
+    else
+        uc = u - 1 - sz;
+    end
+    if (v <= sz/2)
+        vc = v-1;
+    else
+        vc = v - 1 - sz;
+    end
+    
+    wavelength = 1/sqrt(uc*uc + vc*vc); %Replaced 
+    amplitude = abs(Fhat(u,v)/sz); %Replaced
+
+    subplot(3,2,2);
+    showgrey(fftshift(Fhat));
+    title(sprintf('centered Fhat: (uc,vc) = (%d, %d)',uc,vc));
+    
+    subplot(3,2,3);
+    showgrey(real(F),64,-Fabsmax,Fabsmax);
+    title('real(F)');
+    
+    subplot(3,2,4);
+    showgrey(imag(F),64,-Fabsmax,Fabsmax);
+    title('imag(F)');
+    
+    subplot(3,2,5);
+    showgrey(abs(F),64,-Fabsmax,Fabsmax);
+    title(sprintf('abs(F) (amplitude %f)', amplitude));
+    
+    subplot(3,2,6);
+    showgrey(angle(F),64,-pi,pi);
+    title(sprintf('angle(F) (wavelength %f)', wavelength));
+    
+end
+
